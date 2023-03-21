d2125d400265712945590b68035b0a5cd8a21711
Jeremy Yiu <jeremyy@usc.edu>
2023-03-21 00:31:32 -0700

    Finished equalpaths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,4 +1,6 @@
 #ifndef RECCHECK
+#include <utility>
+#include <algorithm>
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
 
 #endif
@@ -7,12 +9,26 @@
 using namespace std;
 
 
-// You may add any prototypes of helper functions here
+std::pair<int,bool> pathCounter(Node * root);
 
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
-
+    return pathCounter(root).second;
 }
 
+std::pair<int,bool> pathCounter(Node * root){
+    if(root == nullptr || root->left == nullptr && root->right == nullptr){
+        return std::pair<int,bool>(0,true);
+    }
+    std::pair<int,bool> left = pathCounter(root->left);
+    std::pair<int,bool> right = pathCounter(root->right);
+    if(!left.second  || !right.second){
+        return std::pair<int,bool>(std::max(left.first,right.first),false);
+    }
+    if(left.first != 0 && right.first != 0){
+        if(left.first != right.first){
+            return std::pair<int,bool>(std::max(left.first,right.first),false);
+        }
+    }
+}
